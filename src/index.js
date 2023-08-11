537659bb8390d9d7d5d727472377517d82a146ba
Sandy <sandeeppatidar082@gmail.com>
2023-08-11 19:55:57 +0530

    changed server

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { createContext, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
-export const server = "http://localhost:4000";
+export const server = "https://community-ymkm.onrender.com";
 
 
 export const Context = createContext({ isAuthenticated : false}) ;
