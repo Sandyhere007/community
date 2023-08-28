e3cb8bc125ac18a06e16ce3ca3928725762b4295
Sandy <sandeeppatidar082@gmail.com>
2023-08-28 23:30:27 +0530

    Updated Myblogs.jsx changed url or image

--- src/components/blogs/BlogPost.jsx
+++ src/components/blogs/BlogPost.jsx
@@ -24,7 +24,7 @@ const BlogPost = () => {
 
   }
 
-  if (!isAuthenticated) return <Navigate to={"/"} />
+  // if (!isAuthenticated) return <Navigate to={"/"} />
   return (
     <>
       <BlogNav />
